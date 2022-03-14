ddb3c093cb0b4583e512920711ee5555c6db8e1a
Hannah Terry <hterry@berkeley.edu>
2022-03-13 19:49:48 -0700

    Add app.yaml with required Python version and add static css files

---
+++ static/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hannah Kaye Web App</h1>
+    </div>
+  );
+}
+
+export default App;
